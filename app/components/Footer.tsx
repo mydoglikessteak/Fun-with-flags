d1695bc2091cf86f42c25b90de8a833ead1301b9
vini <viniciusalex0303@gmail.com>
2025-01-04 11:39:01 -0300

    using talwind

---
+++ app/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () =>{
+    return(
+        <footer className="py-6 mb-8">
+           <p className="text-center">
+           feito pelo brabo Vinicius Alexandre        
+            </p>   </footer>
+        )
+}
+
+export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
-
+import {Footer,Header} from "./components/index"
 export default function Home() {
   return (   
     <>
-    <header>
-      This is the header
-    </header>
+    <Header />
     <main className="flex-1">
       this is the main
     </main>
-    <footer>this is the footer
-
-    </footer>
+   <Footer />
     </>
   );
 }
